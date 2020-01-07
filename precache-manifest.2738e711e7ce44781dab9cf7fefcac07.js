7aa3a52896f5f90ec52c4fdf2b1efda8e152e557
Pau Bonilla <paubonilla@Paus-MacBook-Pro.local>
2020-01-06 22:20:34 -0800

    Updates

---
+++ precache-manifest.2738e711e7ce44781dab9cf7fefcac07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16500e52b632011cc951d0ea318e2344",
+    "url": "/twitter/index.html"
+  },
+  {
+    "revision": "fcb2ef9aff17c6493fb7",
+    "url": "/twitter/static/css/main.195c21aa.chunk.css"
+  },
+  {
+    "revision": "6505753dba8192c1d328",
+    "url": "/twitter/static/js/2.6f339e50.chunk.js"
+  },
+  {
+    "revision": "fcb2ef9aff17c6493fb7",
+    "url": "/twitter/static/js/main.9d275419.chunk.js"
+  },
+  {
+    "revision": "4362e7874eb067a60c82",
+    "url": "/twitter/static/js/runtime-main.444bdfe8.js"
+  }
+]);
